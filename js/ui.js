2388cd9b71d995e84eb479b2188ace416c6b49f8
Carl <carltracy@gmail.com>
2013-05-01 13:18:32 -0400

    actually added all the files this time

---
+++ js/ui.js
@@ -0,0 +1,11 @@
+if (typeof jQuery == 'undefined') {
+    document.write(unescape("%3Cscript src='script/jquery-1.9.1.min.js' type='text/javascript'%3E%3C/script%3E"));
+/*    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);    */        
+}
+
