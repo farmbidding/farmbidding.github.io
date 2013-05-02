93fefd8fa4b6b4089eb5b64b0520e053154369f7
Carl <carltracy@gmail.com>
2013-05-02 13:44:25 -0400

    added ie link

--- js/util.js
+++ js/util.js
@@ -7,7 +7,7 @@
 */
 /* 'Great+Vibes::latin', 'Headland+One::latin', 'Ovo::latin', 'Copse::latin', 'Tangerine:700:latin', 'Squada+One::latin', 'Arizonia::latin', 'Cutive::latin', 'Fjalla+One::latin'  */
 var WebFontConfig = {                
-        google: { families: [  'Great+Vibes::latin', 'Headland+One::latin', 'Cutive::latin' ] }
+        google: { families: [  'Anton::latin', 'Headland+One::latin', 'Cutive::latin' ] }
     };
     (function() {
     var wf = document.createElement('script');
