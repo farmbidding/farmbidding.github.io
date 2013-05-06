c760cf4e9931f47213d962388c3cc43b926717f2
Carl <carltracy@gmail.com>
2013-05-06 16:00:21 -0400

    testing ajax nav

--- js/ui.js
+++ js/ui.js
@@ -35,34 +35,36 @@ function refreshBoth() {
     refreshSH();
 }
 
-$(document).ready(function() {
-    refresh();
-    refreshSH();
-    setInterval(refresh,30000);
-    
-    $('#refresh').click(refreshBoth);
-    
-    $('#nav a').click(function(e) {
-          var url = $(this).attr("href");
-     
-          //This function would get content from the server and insert it into the id="content" element
-          // $.getJSON("content.php", {contentid : url},function (data) {
-          //       $("#content").html(data);
-          // });
-          
-          var tgt = $("#content");
-          if (tgt) {
-              tgt.fadeOut(200).load(url).fadeIn(200);
-              
-              //This is where we update the address bar with the 'url' parameter
-              window.history.pushState('object', 'New Title', url);
+$(document).ready(
+    function() {
+        refresh();
+        refreshSH();
+        setInterval(refresh,30000);
+        
+        $('#refresh').click(refreshBoth);
+        
+        $('#nav a').click(function(e) {
+              var url = $(this).attr("href");
          
-              //This stops the browser from actually following the link
-              e.preventDefault();
-          
-          }
-     
-        }    
+              //This function would get content from the server and insert it into the id="content" element
+              // $.getJSON("content.php", {contentid : url},function (data) {
+              //       $("#content").html(data);
+              // });
+              
+              var tgt = $("#content");
+              if (tgt) {
+                  tgt.fadeOut(200).load(url).fadeIn(200);
+                  
+                  //This is where we update the address bar with the 'url' parameter
+                  window.history.pushState('object', 'New Title', url);
+             
+                  //This stops the browser from actually following the link
+                  e.preventDefault();
+              
+              }
+         
+            }
+        );
     
 });
 
